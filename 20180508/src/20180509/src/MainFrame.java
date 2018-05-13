4161617b68e7d659d91b4f4811b56a05118c781f
20180403 <32056048+sogo0806@users.noreply.github.com>
2018-05-14 01:09:31 +0800

    Add files via upload

---
+++ 20180508/src/20180509/src/MainFrame.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.Objects;
+import java.util.Random;
+
+public class MainFrame extends JFrame {
+    private JButton jbutge = new JButton("Generate");
+    private JButton jbutexit = new JButton("Exit");
+    private JLabel jlb [] = new JLabel[6];
+    private Random rand = new Random();
+    private Container cp;
+    private JPanel jpnn = new JPanel(new GridLayout(1,6,3,3));
+    private JPanel jpnc = new JPanel(new GridLayout(1,2,3,3));
+    boolean flag = false;
+    public MainFrame (){
+        init();
+    }
+    private void init(){
+        setBounds(100,100,600,300);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        cp = this.getContentPane();
+        jpnc.add(jbutge);
+        jpnc.add(jbutexit);
+        cp.add(jpnn,BorderLayout.NORTH);
+        cp.add(jpnc,BorderLayout.CENTER);
+
+        for (int i =0 ; i < 6 ;i++){
+            jlb[i] = new JLabel();
+            jlb[i].setFont(new Font(null,Font.BOLD,32));
+            jpnn.add(jlb[i]);
+        }
+
+
+        jbutge.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                for (int i = 0 ; i<6; i++){
+                    jlb[i].setText(Integer.toString(rand.nextInt(49)+1));
+                    for (int j = 0 ; j < i; j++){
+                        if (jlb[i].getText().equals(jlb[j].getText())){
+                            jlb[i].setText(Integer.toString(rand.nextInt(49)+1));
+                        }
+                    }
+                }
+            }
+    });
+
+            jbutexit.addActionListener(new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    System.exit(0);
+                }
+            });
+
+    }
+}
